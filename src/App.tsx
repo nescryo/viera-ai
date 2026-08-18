a6d241a8bf0af9839e888e5514edffbf217d78aa
nescryo <muhammadryo15@gmail.com>
2026-08-18 13:31:14 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -277,6 +277,7 @@ export function App() {
           cancelAnimationFrame(updateFrameId);
           updateFrameId = null;
         }
+        setIsLoading(false);
         const { emotions, actions, jaText, enText } = parseDualOutputResponse(fullText);
         const activeEmotion = emotions.length > 0 ? emotions[emotions.length - 1] : 'happy';
         setCurrentEmotion(activeEmotion);
